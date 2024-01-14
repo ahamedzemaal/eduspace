72a2399e1877619aacb93aeae998ae6d9bcb7b17
ahamedzemaal <zemaalahamed11@gmail.com>
2024-01-14 20:10:54 +0530

    Add JS and Styling

---
+++ script.js
@@ -0,0 +1,12 @@
+var sidenav = document.querySelector(".side-navbar")
+sidenav.style.display = "none"
+
+function showNavbar() {
+    // sidenav.style.left = "0%"
+    sidenav.style.display ="block"
+}
+
+function closeNavbar() {
+    // sidenav.style.left = "-60%"
+    sidenav.style.display ="none"
+}
