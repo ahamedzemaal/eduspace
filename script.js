2be4db0604dc3052f7005d32ef89ff836095b5fc
ahamedzemaal <zemaalahamed11@gmail.com>
2024-01-26 21:18:41 +0530

    Design

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-var sidenav = document.querySelector(".side-navbar")
+var sidenav = document.getElementById("sidenav")
+var menubtn = document.getElementById("menubtn")
+var closebtn = document.getElementById("closebtn")
+
 sidenav.style.display = "none"
 
 function showNavbar() {
-    // sidenav.style.left = "0%"
+    // sidenav.style.top = "0%"
     sidenav.style.display ="block"
+    menubtn.style.display = "none"
+    closebtn.style.display = "block"
+
 }
 
 function closeNavbar() {
-    // sidenav.style.left = "-60%"
+    // sidenav.style.top = "-60%"
     sidenav.style.display ="none"
+    menubtn.style.display = "block"
+    closebtn.style.display = "none"
 }
